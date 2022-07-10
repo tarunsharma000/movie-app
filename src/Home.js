8eb7abd90393732b3cb683162a18916c045fcdd3
tarunsharma000 <tarun.sharmad35@gmail.com>
2022-07-10 14:39:31 +0530

    http_to_https

--- src/Home.js
+++ src/Home.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=15436a21`;
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=15436a21`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
